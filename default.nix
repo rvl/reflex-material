e9851a843b0c8ce9a91dc208998ad36c85ba7aa9
Rodney Lorrimar <dev@rodney.id.au>
2017-04-21 08:48:02 +0100

    Add nix from node2nix and cabal2nix, slightly edited

---
+++ default.nix
@@ -0,0 +1,40 @@
+{ nixpkgs ? import <nixpkgs> {}, compiler ? "ghcjs" }:
+
+let
+
+  inherit (nixpkgs) pkgs;
+
+  f = { mkDerivation, base, reflex, reflex-dom, stdenv, text }:
+      mkDerivation {
+        pname = "reflex-material";
+        version = "0.0.1.0";
+        src = ./.;
+        isLibrary = true;
+        isExecutable = true;
+        libraryHaskellDepends = [ base reflex-dom text ];
+        executableHaskellDepends = [ base reflex reflex-dom text ];
+        testHaskellDepends = [ base ];
+        homepage = "https://github.com/alasconnect/reflex-material#readme";
+        license = stdenv.lib.licenses.bsd3;
+      };
+
+  tryReflex = import (pkgs.fetchFromGitHub {
+    owner = "reflex-frp";
+    repo = "reflex-platform";
+    rev = "970e6e678a5a54c2a8e8b90315169aa1079f166e";
+    sha256 = "00sf3204mlcpgamyqrks5mhdl7b59p52krjgs5j5n0axwi4giy6f";
+  }) {};
+
+  haskellPackages = if compiler == "ghcjs"
+                       then tryReflex.ghcjs
+                       else (if compiler == "default"
+                         then pkgs.haskellPackages
+                         else pkgs.haskell.packages.${compiler});
+
+  drv = haskellPackages.callPackage f {};
+
+  nodePackages = (import ./mdc.nix { pkgs = nixpkgs; }).packages;
+
+in
+
+  if pkgs.lib.inNixShell then drv.env else drv
