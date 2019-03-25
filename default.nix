581d3f5bea1322733354ca730c2370c78f1f609f
Rodney Lorrimar <dev@rodney.id.au>
2019-03-25 12:14:31 +1000

    Bump reflex-platform and improve nix

---
+++ default.nix
@@ -0,0 +1,38 @@
+{ pkgs ? reflex-platform.nixpkgs
+, reflex-platform ? import reflex-platform-src {}
+, reflex-platform-src ? ./reflex-platform
+}:
+
+let
+  overlay = self: super: {
+    reflex-material = self.callPackage ./reflex-material.nix {};
+  };
+
+  ghc = reflex-platform.ghc.extend overlay;
+  ghcjs = reflex-platform.ghcjs.extend overlay;
+
+  mkShell = haskellPackages: haskellPackages.shellFor {
+    packages = p: [ p.reflex-material ];
+    withHoogle = true;
+  };
+
+  nodePackages = (import ./mdc.nix { inherit pkgs; }).package;
+  addNodeModules = shellDrv: shellDrv.overrideAttrs (oldAttrs: {
+    # Provide NODE_MODULES in the nix-shell.
+    # This will be used by the shake build script.
+    shellHook = (oldAttrs.shellHook or "") + ''
+      export NODE_MODULES=${nodePackages}/lib/node_modules/reflex-material/node_modules
+    '';
+  });
+
+in {
+  inherit ghc ghcjs;
+
+  shells = {
+    ghc = mkShell ghc;
+    ghcjs = mkShell ghcjs;
+  };
+
+  inherit reflex-platform pkgs;
+  inherit nodePackages;
+}
--- shell.nix
+++ shell.nix
@@ -1,33 +1,4 @@
-# --argstr compiler could be ghcjs or ghc
-{ nixpkgs ? import <nixpkgs> {}
-, compiler ? "ghc"
-}:
 let
-
-  inherit (nixpkgs) pkgs;
-
-  tryReflex = import (pkgs.fetchFromGitHub {
-    owner = "reflex-frp";
-    repo = "reflex-platform";
-    rev = "b7c00b3574d0ef42974eda0f2812c794c7b5d4f3";
-    sha256 = "1jfz17y2fq051caby4y4aslxrpvgwwa30ivfw0l5wn5pp5zlrpad";
-  }) {};
-
-  # compiler could be ghcjs or ghc
-  haskellPackages = tryReflex.${compiler};
-
-  drv = haskellPackages.callPackage ./reflex-material.nix {};
-
-  nodePackages = (import ./mdc.nix { pkgs = nixpkgs; }).package;
-
-  # Provide NODE_MODULES in the nix-shell.
-  # This will be used by the shake build script.
-  env' = pkgs.lib.overrideDerivation drv.env (attrs: {
-    shellHook = attrs.shellHook + ''
-      export NODE_MODULES=${nodePackages}/lib/node_modules/reflex-material/node_modules
-    '';
-  });
-
+  proj = import ./. {};
 in
-
-  if pkgs.lib.inNixShell then env' else drv
+  proj.shells.ghc
